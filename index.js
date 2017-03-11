a61202628e75aea9e1309cd3e86835dc8a2e16d1
mozair <macskeptic@gmail.com>
2017-03-11 22:36:47 +0100

    dativ

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ q('.fall--nominativ .pronomen .personal').appendChild(pronomenListe(nominativ.pr
 q('.fall--nominativ .pronomen .possessiv').appendChild(pronomenListe(nominativ.pronomen.possessiv));
 q('.fall--nominativ .pronomen .relativ').appendChild(pronomenListe(nominativ.pronomen.relativ));
 
-q('.fall--akkusativ .pronomen .reflexiv').appendChild(pronomenListe(akkusativ.pronomen.reflexiv));
 q('.fall--akkusativ .pronomen .personal').appendChild(pronomenListe(akkusativ.pronomen.personal));
 q('.fall--akkusativ .pronomen .possessiv').appendChild(pronomenListe(akkusativ.pronomen.possessiv));
 q('.fall--akkusativ .pronomen .relativ').appendChild(pronomenListe(akkusativ.pronomen.relativ));
+q('.fall--akkusativ .pronomen .reflexiv').appendChild(pronomenListe(akkusativ.pronomen.reflexiv));
 
+q('.fall--dativ .pronomen .personal').appendChild(pronomenListe(dativ.pronomen.personal));
+q('.fall--dativ .pronomen .possessiv').appendChild(pronomenListe(dativ.pronomen.possessiv));
 q('.fall--dativ .pronomen .reflexiv').appendChild(pronomenListe(dativ.pronomen.reflexiv));
 q('.fall--dativ .pronomen .relativ').appendChild(pronomenListe(dativ.pronomen.relativ));
