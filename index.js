09d964bd46cdf9f512153d7ef3e7f85f44d7cc13
mozair <macskeptic@gmail.com>
2017-03-11 21:58:53 +0100

    relativ akkusativ

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ q('.fall--nominativ .pronomen .possessiv').appendChild(pronomenListe(nominativ.p
 q('.fall--nominativ .pronomen .relativ').appendChild(pronomenListe(nominativ.pronomen.relativ));
 
 q('.fall--akkusativ .pronomen .reflexiv').appendChild(pronomenListe(akkusativ.pronomen.reflexiv));
+q('.fall--akkusativ .pronomen .personal').appendChild(pronomenListe(akkusativ.pronomen.personal));
+q('.fall--akkusativ .pronomen .possessiv').appendChild(pronomenListe(akkusativ.pronomen.possessiv));
+q('.fall--akkusativ .pronomen .relativ').appendChild(pronomenListe(akkusativ.pronomen.relativ));
 
 q('.fall--dativ .pronomen .reflexiv').appendChild(pronomenListe(dativ.pronomen.reflexiv));
